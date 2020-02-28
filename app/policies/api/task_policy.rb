4016a60ef78f760d08e4ce471af14b5e618c416b
sian stone <sian.m.stone@gmail.com>
2020-02-28 16:18:15 +0000

    added policies for colour_theme, panel & task

---
+++ app/policies/api/task_policy.rb
@@ -0,0 +1,23 @@
+class Api::TaskPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    is_owner?
+  end
+
+  def destroy?
+    is_owner?
+  end
+
+  private
+
+  def is_owner?
+    user == record.user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,21 +15,9 @@ Rails.application.routes.draw do
       end
     end
 
-    get 'panels', to: 'panels#index'
-    post 'panel', to: 'panels#create'
-    patch 'panel', to: 'panels#update'
-
-    # post 'colour_theme', to: 'colour_theme#create'
-    # delete 'colour_theme', to: 'colour_theme#destroy'
-    # patch 'colour_theme', to: 'colour_theme#update'
-
     resources :tasks, only: [:index, :create, :update, :destroy]
     resources :colour_themes, only: [:index, :create, :update, :destroy]
     resources :panels, only: [:index, :create, :update, :destroy]
-    # get 'tasks', to: 'tasks#index'
-    # post 'task', to: 'tasks#create'
-    # delete 'task', to: 'tasks#destroy'
-
 
   end
 end
